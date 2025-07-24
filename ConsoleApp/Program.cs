c48c61646d73ee0784c60bba6a7d770ac81e531c
RichardBurkhill <RichardBurkhill4@gmail.com>
2025-07-24 22:53:40 +0100

    Example P/Inoke function

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        [DllImport("libnative.dylib", EntryPoint = "add_numbers")]
+        public static extern int AddNumbers(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int result = AddNumbers(5, 7);
+            Console.WriteLine($"5 + 7 = {result}");
+        }
+    }
+}
---
+++ NativeLib/native.cpp
@@ -0,0 +1,9 @@
+// native.cpp
+#include <iostream>
+
+extern "C" {
+    int add_numbers(int a, int b) {
+        return a + b;
+    }
+}
+
